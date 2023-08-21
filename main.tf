59b4f2dedd54bccb8590e334815992fc393307fd
Moein <moein.ghobbeh@gmail.com>
2023-08-21 19:22:39 +0200

    debug adding policy to the s3 bucket

--- main.tf
+++ main.tf
@@ -13,8 +13,16 @@ resource "aws_s3_bucket" "finance" {
 resource "aws_s3_bucket_object" "finance-2020" {
   content  ="c:/text.txt"
   key = "text.txt"
-  #  policy = ""
   bucket   = aws_s3_bucket.finance.id
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.finance.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 data "aws_iam_group" "finance-date" {
@@ -23,27 +31,19 @@ data "aws_iam_group" "finance-date" {
 
 resource "aws_s3_bucket_policy" "finance-policy" {
   bucket = aws_s3_bucket.finance.id
-  policy = <<EOF
-        {
-        "Version": "2012-10-17",
-              "Statement": [
-                    {
-                        "Action": "*" ,
-                        "Effect": "Allow" ,
-                        "Resource": "arn:aws:s3:::${aws_s3_bucket.finance.id}/*",
-                        "Principal": {
-                            "aws": [
-                                    "${data.aws_iam_group.finance-date.arn}"
-                                    ]
-                            }
-                    }
-                  ]
-        }
-            EOF
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "*",
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.finance.id}/*"
+        Principal: "*"
+      },
+    ],
 
+  })
+  depends_on = [aws_s3_bucket_public_access_block.example]
 }
-
-
-
 
 
